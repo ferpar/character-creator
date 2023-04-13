2cfc83da672ea85be19870c054c89bc79c716938
fer <ferp@protonmail.ch>
2023-04-13 11:18:25 +0200

    initial attempt

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <div
+        style={{
+          position: "fixed",
+          bottom: 0,
+          "background-color": "hsl(195, 20%, 86%)",
+          width: "100%",
+          height: "40%",
+          "z-index": "-1",
+        }}
+      ></div>
       <CharacterEditor />
       <Footer />
     </>
